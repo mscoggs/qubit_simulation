badc842a7e1bf0071577eaaf78de06223b547e20
mscoggs <scoggim@wwu.edu>
2018-07-30 11:46:20 -0700

    .

--- ctest.cpp
+++ ctest.cpp
@@ -6,9 +6,13 @@
 
 
 int main(){
-  unsigned long long i, j;
-  i = 7;
-  j = 8;
-      printf("%i", i ^ j);
+  unsigned long long int p,i, z_mask,*a;
+  unsigned long long int vtemp;
+  int z_count = 0;
+  if((1ULL<<4) & (1ULL<<4))
+    z_count += 7;
+  if((1ULL<<3) & (1ULL<<4))
+    z_count += 5;
+  printf("%i", z_count);
 
 }
